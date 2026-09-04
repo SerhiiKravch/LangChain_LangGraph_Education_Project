a97cfb14acb2c59871bcac2400a6346085556c78
SerhiiKravch <kravchyshynserg@gmail.com>
2026-09-04 17:38:44 +0200

    feat: add risk assessment schema and decision labels

--- src/support_agent/schemas/__init__.py
+++ src/support_agent/schemas/__init__.py
@@ -2,11 +2,15 @@
 
 from support_agent.schemas.classification import TicketClassification
 from support_agent.schemas.draft import DraftCitation, DraftResponse, SourceSnippet
+from support_agent.schemas.risk import RiskAssessment, RiskLevel, RoutingDecision
 from support_agent.schemas.ticket import SupportTicket, TicketCategory, TicketInput
 
 __all__ = [
     "DraftCitation",
     "DraftResponse",
+    "RiskAssessment",
+    "RiskLevel",
+    "RoutingDecision",
     "SupportTicket",
     "SourceSnippet",
     "TicketCategory",
